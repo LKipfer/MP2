34974ccf5063c74b2a94715d86af63a0f9138974
LKipfer <72979191+LKipfer@users.noreply.github.com>
2021-01-02 16:52:26 +0100

    learning

--- src/client.java
+++ src/client.java
@@ -1,4 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class client {
 
+    public static void main(String[] args) throws IOException {
+     try  (
+           Socket s = new Socket("localhost",4999);
+           OutputStreamWriter out = new OutputStreamWriter(s.getOutputStream());
+           BufferedReader inReader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+          ) {
+            //GET etc.
+            //Remember \r\n because it marks the end of a line.
+            //At the very end of the HTTP request we have to use \r\n\r\n as it ends the http request.
+            }
+     catch (IOException e) {
+         e.printStackTrace();
+     }
+
+
+    }
 
 }
